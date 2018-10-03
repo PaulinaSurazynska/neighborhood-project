87cbdff22467b5a22d3790c62c61f7fbfff9d4f0
Paulina Surazynska <paulinasurazynska@standingongiants.com>
2018-10-03 21:08:38 +0100

    fixes after feedback

--- js/app.js
+++ js/app.js
@@ -4,59 +4,91 @@ var ViewModel = function(){
   self.museumList = ko.observableArray(museums);
 
   // show marker for the coresponding museumItem from the list
-    this.showMarker = function(museumItem) {
-      google.maps.event.trigger(this.marker, 'click');
-    }
+  this.showMarker = function(museumItem) {
+    google.maps.event.trigger(this.marker, 'click');
+  }
 
   // create museumItem for each museum from the array
   self.museumList().forEach(function(museumItem){
 
-    // create a marker for each museum
-    var marker = new google.maps.Marker({
-     map: map,
-     name: museumItem.name,
-     address: museumItem.vicinity,
-     position: museumItem.geometry.location,
-     animation: google.maps.Animation.DROP,
-   });
+  // create a marker for each museum
+  var marker = new google.maps.Marker({
+    map: map,
+    name: museumItem.name,
+    position: museumItem.location,
+    animation: google.maps.Animation.DROP,
+  });
 
-   // create content for each infoWindow
-   var contentString = '<h5>'+ museumItem.name +'</h5>' + '<h6>'+ museumItem.vicinity + '</h6>'
+  // call foursquare for each museum
+  getDataFromFoursquare(museumItem)
 
-    // bind marker to the museum
-    museumItem.marker = marker
+  // bind marker to the museum
+  museumItem.marker = marker
 
-    // and open infowindow when clicked
-    marker.addListener('click', function() {
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function() {
-        marker.setAnimation(null);
-      }, 750);
-      infoWindow.setContent(contentString);
-      infoWindow.open(map, marker);
+  // and open infowindow when clicked
+  marker.addListener('click', function() {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function() {
+      marker.setAnimation(null);
+    }, 750);
+    infoWindow.setContent(museumItem.infoMarkerWindow);
+    infoWindow.open(map, marker);
     });
   });
 
-    // create infoWindow for museum marker
-    var infoWindow = new google.maps.InfoWindow({
-      content: self.contentString
-    });
+  // create infoWindow for museum marker
+  var infoWindow = new google.maps.InfoWindow();
 
-    // Create an observable for filter museum
-    self.museumFilter = ko.observable("");
+  // api request to foursquare
+  function getDataFromFoursquare(museum){
+    // define variables used in url HTTP request
+    var urlBeginning = "https://api.foursquare.com/v2/venues/";
+    var museumID = museum.id;
+    var client_id = "?client_id=HPDRNOUEPAXSNMMECXU0FTUWLXVNKVQFG10BPBQ4F44ZWWR4&";
+    var client_secret = "client_secret=EAX3GEJCFYYB3XSY43PDHIQW1XAWKE5UEELXI0QMRYPIDSY1&v=20181003";
+    var v = "&v=20181003";
 
-    // Create filter input function to update filter list
-    self.museumFilterList = ko.computed(function() {
-      return ko.utils.arrayFilter(self.museumList(), function(museum) {
-        var visible = museum.name.toLowerCase().indexOf(self.museumFilter().toLowerCase()) != -1;
-        if (visible) {
-          museum.marker.setVisible(visible);
-        } else {
-          museum.marker.setVisible(false);
+    var urlFull = urlBeginning + museumID + client_id + client_secret + v;
+
+    // create request
+    $.ajax({
+      url: urlFull,
+      success: function(response) {
+        // get info from the response
+        var name = response.response.venue.name
+        var desc = response.response.venue.description;
+        var address = response.response.venue.location.formattedAddress;
+        var opening = response.response.venue.hours.status;
+        var web = response.response.venue.url;
+
+        museum.infoMarkerWindow =
+          '<h5>'+ name + '</h5>' +
+          '<p>' + desc + '</p>' +
+          '<span>' + address + '</span>' +
+          '<span>' + opening + '</span>' +
+          '<a target="_blank">' + web + '</a>'
+        },
+        error: function() {
+          museum.infoMarkerWindow = 'Something went wrong and we cannot load data from fourSquare';
         }
-        return visible;
       });
+    };
+
+  // Create an observable for filter museum
+  self.museumFilter = ko.observable("");
+
+  // Create filter input function to update filter list
+  self.museumFilterList = ko.computed(function() {
+    return ko.utils.arrayFilter(self.museumList(), function(museum) {
+      var visible = museum.name.toLowerCase().indexOf(self.museumFilter().toLowerCase()) != -1;
+      if (visible) {
+        museum.marker.setVisible(visible);
+      } else {
+        museum.marker.setVisible(false);
+      }
+      return visible;
     });
+  });
 }
 
 // define global variables used within initMap function
@@ -69,7 +101,6 @@ var map;
 
 // inistalize google map
 function initMap() {
-
   // create a new map
   map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 51.519103, lng: -0.126345},
--- js/model.js
+++ js/model.js
@@ -2,399 +2,67 @@
 
 var museums = [
   {
-         "geometry" : {
-            "location" : {
-               "lat" : 51.5194133,
-               "lng" : -0.1269566
-            },
-            "viewport" : {
-               "northeast" : {
-                  "lat" : 51.52145429999999,
-                  "lng" : -0.12477735
-               },
-               "southwest" : {
-                  "lat" : 51.51732029999999,
-                  "lng" : -0.12914235
-               }
-            }
-         },
-         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/museum-71.png",
-         "id" : "ad6aaec7b7b0fa2c97a127c24845d76135e760ae",
-         "name" : "The British Museum",
-         "opening_hours" : {
-            "open_now" : true
-         },
-         "photos" : [
-            {
-               "height" : 1320,
-               "html_attributions" : [
-                  "\u003ca href=\"https://maps.google.com/maps/contrib/116829866718705237173/photos\"\u003ePentti Koppinen\u003c/a\u003e"
-               ],
-               "photo_reference" : "CmRaAAAAIYPP1FC69OxCCPO5OWIJgfjW8NlzNw2G6ayz8u_TMlPwk_dITGJxGtd7_TDFxUYqxCUtLE9mkpgeRK0R9WwpaEKx5lRva4L9TqjoIbynt-iWzAQRHc1Yd7kj9elVxhZnEhBVs69oaR68afCuXo84YfG_GhTNDwg1PsLLEOXbOM0x4Kbk1TCHKg",
-               "width" : 1980
-            }
-         ],
-         "place_id" : "ChIJB9OTMDIbdkgRp0JWbQGZsS8",
-         "plus_code" : {
-            "compound_code" : "GV9F+Q6 Bloomsbury, London, United Kingdom",
-            "global_code" : "9C3XGV9F+Q6"
-         },
-         "rating" : 4.7,
-         "reference" : "ChIJB9OTMDIbdkgRp0JWbQGZsS8",
-         "scope" : "GOOGLE",
-         "types" : [ "museum", "point_of_interest", "establishment" ],
-         "vicinity" : "Great Russell Street, London"
-      },
+  "location" : {
+    "lat" : 51.5194133,
+    "lng" : -0.1269566
+  },
+  "id" : "4ac518d2f964a5203da720e3",
+  "name" : "The British Museum",
+  },
   {
-         "geometry" : {
-            "location" : {
-               "lat" : 51.5021585,
-               "lng" : -0.1293572
-            },
-            "viewport" : {
-               "northeast" : {
-                  "lat" : 51.50356593029149,
-                  "lng" : -0.127901969708498
-               },
-               "southwest" : {
-                  "lat" : 51.5008679697085,
-                  "lng" : -0.130599930291502
-               }
-            }
-         },
-         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/museum-71.png",
-         "id" : "ded3d4530b9507102bc76a1e81d469449c02dc9f",
-         "name" : "Churchill War Rooms",
-         "opening_hours" : {
-            "open_now" : true
-         },
-         "photos" : [
-            {
-               "height" : 1500,
-               "html_attributions" : [
-                  "\u003ca href=\"https://maps.google.com/maps/contrib/111952430579308001031/photos\"\u003eLuli Andrejerak\u003c/a\u003e"
-               ],
-               "photo_reference" : "CmRZAAAAFzBSRv5EezWNDBGwasofXf6_8g_OYu3JPzZcIkYOJN2Lyv9xAt8v0V6ryt-i8YAnILoGyA0s0R7vFW1BmmrOPGgwu2yynR0K1c_oup6JUoD96si9aHnFU6mPJRHpkJyfEhAq3Zegudg55vvEy0HWbTD4GhTXEaQkGjzQm8zb6pVJlXzvcxNIpg",
-               "width" : 2000
-            }
-         ],
-         "place_id" : "ChIJq4lX1doEdkgR5JXPstgQjc0",
-         "plus_code" : {
-            "compound_code" : "GV2C+V7 Westminster, London, United Kingdom",
-            "global_code" : "9C3XGV2C+V7"
-         },
-         "rating" : 4.6,
-         "reference" : "ChIJq4lX1doEdkgR5JXPstgQjc0",
-         "scope" : "GOOGLE",
-         "types" : [ "museum", "point_of_interest", "establishment" ],
-         "vicinity" : "Clive Steps, King Charles Street, London"
-      },
+  "location" : {
+    "lat" : 51.50887601013219,
+    "lng" : -0.1284778118133545
+  },
+  "id" : "4ac518cdf964a520e6a520e3",
+  "name" : "National Gallery",
+  },
   {
-         "geometry" : {
-            "location" : {
-               "lat" : 51.523767,
-               "lng" : -0.1585557
-            },
-            "viewport" : {
-               "northeast" : {
-                  "lat" : 51.5251386302915,
-                  "lng" : -0.1570715197084979
-               },
-               "southwest" : {
-                  "lat" : 51.5224406697085,
-                  "lng" : -0.159769480291502
-               }
-            }
-         },
-         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/museum-71.png",
-         "id" : "3976b78b5822428ccf0d43d7cbf110e00cbc3fae",
-         "name" : "The Sherlock Holmes Museum",
-         "opening_hours" : {
-            "open_now" : true
-         },
-         "photos" : [
-            {
-               "height" : 1880,
-               "html_attributions" : [
-                  "\u003ca href=\"https://maps.google.com/maps/contrib/111006586262493227533/photos\"\u003eGianandrea Franzoni\u003c/a\u003e"
-               ],
-               "photo_reference" : "CmRaAAAA1QEFdaR7ALFiKWbiOHMIQ5Tgwh3ezcXCLTg39qlKcIdQUaCFYkhTSuPrwps-JXe84WML3-c1Dh2fvzumUTwAdmOkYpAXGMqz9Ai_XJqDrH0scTOGcyd6TRhDshFiaucdEhDFzfwUHCM8bylsEv0-LD5pGhRtu321J6QcdLxIWHNP0z6UQP5yEA",
-               "width" : 2816
-            }
-         ],
-         "place_id" : "ChIJkeddM88adkgR80g3pdkn0vw",
-         "plus_code" : {
-            "compound_code" : "GRFR+GH Marylebone, London, United Kingdom",
-            "global_code" : "9C3XGRFR+GH"
-         },
-         "rating" : 4.2,
-         "reference" : "ChIJkeddM88adkgR80g3pdkn0vw",
-         "scope" : "GOOGLE",
-         "types" : [ "museum", "point_of_interest", "establishment" ],
-         "vicinity" : "221b Baker Street, London"
-      },
+  "location" : {
+    "lat" : 51.49652085547838,
+    "lng" : -0.17653226852416992
+  },
+  "id" : "4ac518cdf964a520e8a520e3",
+  "name" : "Natural History Museum",
+  },
   {
-         "geometry" : {
-            "location" : {
-               "lat" : 51.5230457,
-               "lng" : -0.1543453
-            },
-            "viewport" : {
-               "northeast" : {
-                  "lat" : 51.5241921302915,
-                  "lng" : -0.153265319708498
-               },
-               "southwest" : {
-                  "lat" : 51.5214941697085,
-                  "lng" : -0.155963280291502
-               }
-            }
-         },
-         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/museum-71.png",
-         "id" : "abfa2e61a6cc1110366e53813cf5ed9acad0dc40",
-         "name" : "Madame Tussauds London",
-         "opening_hours" : {
-            "open_now" : false
-         },
-         "photos" : [
-            {
-               "height" : 1363,
-               "html_attributions" : [
-                  "\u003ca href=\"https://maps.google.com/maps/contrib/106653915766133740130/photos\"\u003eJ N\u003c/a\u003e"
-               ],
-               "photo_reference" : "CmRaAAAAUThrxV8fTzCDsi3I_e2OQuEqCOgX08W762AR6PDkdVkieYhe3HC8Bq_xByqTse-qPpGOp7ALc6rEJ0qqlhjVEN3yWWlBd46rwn6PPTlbYWm2nKDhbpACaDkNGK4eakCDEhBlOdZ1BPAYx0LOssBLz9PbGhTvsJ5YqWtBjo0xfldvxRXivlV6Yg",
-               "width" : 2048
-            }
-         ],
-         "place_id" : "ChIJgZ24Us4adkgRpDNAwNPO_SY",
-         "plus_code" : {
-            "compound_code" : "GRFW+67 Marylebone, London, United Kingdom",
-            "global_code" : "9C3XGRFW+67"
-         },
-         "rating" : 4.3,
-         "reference" : "ChIJgZ24Us4adkgRpDNAwNPO_SY",
-         "scope" : "GOOGLE",
-         "types" : [ "museum", "point_of_interest", "establishment" ],
-         "vicinity" : "Marylebone Road, London"
-      },
+  "location" : {
+    "lat" : 51.50766916206692,
+    "lng" : -0.0998880232892575
+  },
+  "id" : "4ac518d3f964a52076a720e3",
+  "name" : "Tate Modern",
+  },
   {
-         "geometry" : {
-            "location" : {
-               "lat" : 51.5176183,
-               "lng" : -0.09677819999999998
-            },
-            "viewport" : {
-               "northeast" : {
-                  "lat" : 51.5189363302915,
-                  "lng" : -0.09399285000000003
-               },
-               "southwest" : {
-                  "lat" : 51.5162383697085,
-                  "lng" : -0.09770664999999996
-               }
-            }
-         },
-         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/museum-71.png",
-         "id" : "1cc11ff9ddfd51dad074ba70282851a2428ba560",
-         "name" : "Museum of London",
-         "opening_hours" : {
-            "open_now" : true
-         },
-         "photos" : [
-            {
-               "height" : 1200,
-               "html_attributions" : [
-                  "\u003ca href=\"https://maps.google.com/maps/contrib/118159590090857326183/photos\"\u003eIngrid van Vliet\u003c/a\u003e"
-               ],
-               "photo_reference" : "CmRaAAAAZIBQRgoGTekWPeD2x06InLql58rydBpt37OT012xxJQ5YovbPFt5LAT6QX5g9RqgOSjUt_V0M8PrKA6RraN2FhohYII92Uv7bY44X-e5dXInJEMcgYn0w0xlKeptIUnCEhB4iC9rAnWa9sj3Lp2jInSaGhQGo4b7JEAB5jwWmVCA42Nzi7FFYg",
-               "width" : 1600
-            }
-         ],
-         "place_id" : "ChIJ68vBCFUbdkgR5CUqlcHifUA",
-         "plus_code" : {
-            "compound_code" : "GW93+27 City of London, London, United Kingdom",
-            "global_code" : "9C3XGW93+27"
-         },
-         "rating" : 4.5,
-         "reference" : "ChIJ68vBCFUbdkgR5CUqlcHifUA",
-         "scope" : "GOOGLE",
-         "types" : [ "museum", "point_of_interest", "establishment" ],
-         "vicinity" : "150 London Wall, London"
-      },
+  "location" : {
+    "lat" : 51.52289004567661,
+    "lng" : -0.15484929084777832
+  },
+  "id" : "4ac518cef964a520fca520e3",
+  "name" : "Madame Tussauds",
+  },
   {
-         "geometry" : {
-            "location" : {
-               "lat" : 51.50892899999999,
-               "lng" : -0.128299
-            },
-            "viewport" : {
-               "northeast" : {
-                  "lat" : 51.5103531802915,
-                  "lng" : -0.1274214
-               },
-               "southwest" : {
-                  "lat" : 51.5076552197085,
-                  "lng" : -0.1306586
-               }
-            }
-         },
-         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/museum-71.png",
-         "id" : "28fbb9d350306df75899c4902bb6746ba6c3134d",
-         "name" : "The National Gallery",
-         "opening_hours" : {
-            "open_now" : true
-         },
-         "photos" : [
-            {
-               "height" : 3648,
-               "html_attributions" : [
-                  "\u003ca href=\"https://maps.google.com/maps/contrib/109525122323023908068/photos\"\u003eHans Richter\u003c/a\u003e"
-               ],
-               "photo_reference" : "CmRaAAAAbWa83MedSPeGrDOaJSbBIj6z9zrLtwGsTXU8nHXe63vqvAwME8uZfNFdd0o4kOd07uU18sXMaK5rk5IVPML9azFAm1mU7G_LzHJLyhKITfJzTFT7mAVIR__J6yf97ueIEhA_ArPdMRJTd06XYgJyZ4fMGhSWbVRlnnkpI0YrwarXt0jXa409qw",
-               "width" : 5472
-            }
-         ],
-         "place_id" : "ChIJeclqF84EdkgRtKAjTmWFr0I",
-         "plus_code" : {
-            "compound_code" : "GV5C+HM Westminster, London, United Kingdom",
-            "global_code" : "9C3XGV5C+HM"
-         },
-         "rating" : 4.7,
-         "reference" : "ChIJeclqF84EdkgRtKAjTmWFr0I",
-         "scope" : "GOOGLE",
-         "types" : [ "art_gallery", "museum", "point_of_interest", "establishment" ],
-         "vicinity" : "Trafalgar Square, London"
-      },
+  "location" : {
+    "lat" : 51.50902291614178,
+    "lng" : -0.13952851295471191
+  },
+  "id" : "4ac518d3f964a52067a720e3",
+  "name" : "Royal Academy of Arts",
+  },
   {
-         "geometry" : {
-            "location" : {
-               "lat" : 51.511059,
-               "lng" : -0.1171479999999999
-            },
-            "viewport" : {
-               "northeast" : {
-                  "lat" : 51.51258685000001,
-                  "lng" : -0.115871769708498
-               },
-               "southwest" : {
-                  "lat" : 51.50959945,
-                  "lng" : -0.118569730291502
-               }
-            }
-         },
-         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
-         "id" : "15bec6583e30c575cc7a1652b311354c37b43058",
-         "name" : "Somerset House",
-         "opening_hours" : {
-            "open_now" : true
-         },
-         "photos" : [
-            {
-               "height" : 1363,
-               "html_attributions" : [
-                  "\u003ca href=\"https://maps.google.com/maps/contrib/118008305675879084685/photos\"\u003eHappening London\u003c/a\u003e"
-               ],
-               "photo_reference" : "CmRaAAAAw2AicwFh69Ii2f4jBM9259YADHf5fPYjEtyRUSnU4gRoiJxewtAYPy7cDPDK1THcdSL33-2FvmxFQ1qfGrjqVzrNplpK2YTzjHXWsMfuT8KydMK-HT_WuyzAZ6docwbVEhCjGwrukQABgf-hnlbJQzN3GhQPT9gcZ-aU1z9xW6Iak6EGTQ3sng",
-               "width" : 2048
-            }
-         ],
-         "place_id" : "ChIJcWfE87UEdkgRoYClOMTsOQQ",
-         "plus_code" : {
-            "compound_code" : "GV6M+C4 Lambeth, London, United Kingdom",
-            "global_code" : "9C3XGV6M+C4"
-         },
-         "rating" : 4.5,
-         "reference" : "ChIJcWfE87UEdkgRoYClOMTsOQQ",
-         "scope" : "GOOGLE",
-         "types" : [ "museum", "point_of_interest", "establishment" ],
-         "vicinity" : "Strand, London"
-      },
+  "location" : {
+    "lat" : 51.54834414150891,
+    "lng" : -0.17727644221353694
+  },
+  "id" : "4ac518d3f964a52070a720e3",
+  "name" : "Freud Museum",
+  },
   {
-         "geometry" : {
-            "location" : {
-               "lat" : 51.5094236,
-               "lng" : -0.1281216
-            },
-            "viewport" : {
-               "northeast" : {
-                  "lat" : 51.51082753029149,
-                  "lng" : -0.1268277
-               },
-               "southwest" : {
-                  "lat" : 51.50812956970849,
-                  "lng" : -0.1295505
-               }
-            }
-         },
-         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
-         "id" : "7d843fb60b5af0e315697532340d16d5d428245d",
-         "name" : "National Portrait Gallery",
-         "opening_hours" : {
-            "open_now" : true
-         },
-         "photos" : [
-            {
-               "height" : 1280,
-               "html_attributions" : [
-                  "\u003ca href=\"https://maps.google.com/maps/contrib/101233792564302545274/photos\"\u003eLuciano Fiore\u003c/a\u003e"
-               ],
-               "photo_reference" : "CmRaAAAAr5zz6fmlVCKAhf-rGaWWaCyRkN1sDO1PBh5DXLot5EH6sJTGeFzEfZPjQDnDq4t9Pj-zEhGy5xsCbKHShJGDdevWf37yZWvcEWHEfOo4RiJFRbwM0W7vkI-i7pwiPZPTEhDz8DzlNnm1XisPhwEsClAxGhQ7xMcAnkuwfXFdxMgfpGUcDF2jZg",
-               "width" : 1920
-            }
-         ],
-         "place_id" : "ChIJeclqF84EdkgRYkL4mtJ7rLM",
-         "plus_code" : {
-            "compound_code" : "GV5C+QQ Westminster, London, United Kingdom",
-            "global_code" : "9C3XGV5C+QQ"
-         },
-         "rating" : 4.6,
-         "reference" : "ChIJeclqF84EdkgRYkL4mtJ7rLM",
-         "scope" : "GOOGLE",
-         "types" : [ "art_gallery", "museum", "point_of_interest", "establishment" ],
-         "vicinity" : "Saint Martin's Place, London"
-      },
-  {
-         "geometry" : {
-            "location" : {
-               "lat" : 51.4910621,
-               "lng" : -0.1277886
-            },
-            "viewport" : {
-               "northeast" : {
-                  "lat" : 51.4920134302915,
-                  "lng" : -0.125835969708498
-               },
-               "southwest" : {
-                  "lat" : 51.48931546970851,
-                  "lng" : -0.128533930291502
-               }
-            }
-         },
-         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
-         "id" : "73e4a69a2f819a9360706929452dddb527347097",
-         "name" : "Tate Britain",
-         "opening_hours" : {
-            "open_now" : true
-         },
-         "photos" : [
-            {
-               "height" : 1080,
-               "html_attributions" : [
-                  "\u003ca href=\"https://maps.google.com/maps/contrib/105787647052433885325/photos\"\u003eJonathan Letzter\u003c/a\u003e"
-               ],
-               "photo_reference" : "CmRaAAAAAK05H443e1ennFKY8kJ8sWQl59HXzZiqI7OvrpLWj7IoIA1eQpCMXgB4XoYdobHZ4DA7zigkfOV-vqMWfeKd5Mlw6-kdARO7WF4Yo_fW0nrPUPimBrYXPaRQH-mEPXOvEhCEjgKkE47dgni6PZrpRLJPGhR4_r0x1WoivSgK_pSd-EuSeracmQ",
-               "width" : 1440
-            }
-         ],
-         "place_id" : "ChIJvWjCxekEdkgRoCgQVJHZH_U",
-         "plus_code" : {
-            "compound_code" : "FVRC+CV Westminster, London, United Kingdom",
-            "global_code" : "9C3XFVRC+CV"
-         },
-         "rating" : 4.6,
-         "reference" : "ChIJvWjCxekEdkgRoCgQVJHZH_U",
-         "scope" : "GOOGLE",
-         "types" : [ "art_gallery", "museum", "point_of_interest", "establishment" ],
-         "vicinity" : "Millbank, London"
-      },
+  "location" : {
+    "lat" : 51.517565,
+    "lng" : -0.09685
+  },
+  "id" : "4ac518d2f964a5203ca720e3",
+  "name" : "Museum of London",
+  },
 ]
